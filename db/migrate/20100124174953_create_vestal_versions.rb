ba927502bafbb55830c6e49004b0112dd50c08d4
Javier Blanco <jbgutierrez@gmail.com>
2010-01-27 15:41:53 +0100

    actualización de vestal_versions a 1.0

--- db/migrate/20100124174953_create_vestal_versions.rb
+++ db/migrate/20100124174953_create_vestal_versions.rb
@@ -2,14 +2,21 @@ class CreateVestalVersions < ActiveRecord::Migration
   def self.up
     create_table :versions do |t|
       t.belongs_to :versioned, :polymorphic => true
+      t.belongs_to :user, :polymorphic => true
+      t.string :user_name
       t.text :changes
       t.integer :number
-      t.datetime :created_at
+      t.string :tag
+
+      t.timestamps
     end
 
     change_table :versions do |t|
-      t.index [:versioned_type, :versioned_id]
+      t.index [:versioned_id, :versioned_type]
+      t.index [:user_id, :user_type]
+      t.index :user_name
       t.index :number
+      t.index :tag
       t.index :created_at
     end
   end
